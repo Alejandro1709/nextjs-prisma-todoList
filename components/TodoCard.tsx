fbc46266db638953357dd09537bd66f97d5d35c7
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-07-26 18:35:56 -0500

    Edit: todo card

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -7,10 +7,10 @@ type Props = {
 
 function TodoCard({ body, completed }: Props) {
   return (
-    <div className='card'>
+    <div className='card select-none cursor-pointer'>
       <div className='card__wrapper flex justify-between p-4 border bg-white'>
         <p className='text-lg font-semibold'>{body}</p>
-        <button className='text-blue-600'>
+        <button className='text-blue-600 hover:text-blue-700'>
           {completed ? 'Completed' : 'Complete'}
         </button>
       </div>
