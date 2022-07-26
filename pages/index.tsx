e83656d591ef132bce849831a8b093d6f0e8fc97
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-07-26 17:26:34 -0500

    Add loading alert

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,7 @@ const Home: NextPage = () => {
             <input type="text" className="ml-2 w-full outline-none" placeholder="Buy Tomatoes" />
             <input type="submit" className="p-2 ml-auto bg-blue-500 text-white hover:bg-blue-600 cursor-pointer" value="Add Todo"/>
           </form>
+          <p className="mt-4 p-2 bg-blue-400 rounded-md text-white select-none">Loading...</p>
         </section>
       </main>
     </div>
