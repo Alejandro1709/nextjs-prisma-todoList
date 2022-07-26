1516c0a9d575fbe546dc6b3cec82cc959d30e0b8
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-07-26 18:49:43 -0500

    Add: dummy data and rendered them into the todo list

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import TodoCard from './TodoCard';
+import Todo from '../types/todo';
 
-type Props = {};
+const myTodos: Array<Todo> = [
+  {
+    id: 1,
+    body: 'Buy milk for the family',
+    completed: false,
+  },
+  {
+    id: 2,
+    body: 'Finish extended project',
+    completed: false,
+  },
+  {
+    id: 3,
+    body: 'Take the trash out',
+    completed: false,
+  },
+];
 
-function TodoList({}: Props) {
+function TodoList() {
   return (
     <section className='flex flex-col gap-2 px-6 border-l border-r mb-4'>
-      <TodoCard body='Buy milk for the family' />
-      <TodoCard body='Finish individual project' />
-      <TodoCard body='Take the dog out' />
+      {myTodos.length > 0 ? (
+        myTodos.map((todo) => <TodoCard key={todo.id} body={todo.body} />)
+      ) : (
+        <p>No Todos were found...</p>
+      )}
     </section>
   );
 }
