805194f092e52d1e9cf23b5c4225e34782f4087c
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-07-26 21:24:35 -0500

    Add: create todo

--- pages/api/todos.ts
+++ pages/api/todos.ts
@@ -10,10 +10,24 @@ type Data = {
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  try {
-    const todos = await prisma.todo.findMany();
-    return res.status(200).json({ todos: todos });
-  } catch (error) {
-    console.error(error);
+  if (req.method === 'GET') {
+    try {
+      const todos = await prisma.todo.findMany();
+      return res.status(200).json({ todos: todos });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  if (req.method === 'POST') {
+    const { body } = req.body;
+
+    try {
+      const newTodo = await prisma.todo.create({ data: { body } });
+
+      res.status(201).send('Created');
+    } catch (error) {
+      console.error(error);
+    }
   }
 };
