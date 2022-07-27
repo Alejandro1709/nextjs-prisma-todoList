2fdd6171f069c575ba06a1686e6e38620011eeb5
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2022-07-26 20:38:43 -0500

    Add: server side rendering

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -2,29 +2,15 @@ import React from 'react';
 import TodoCard from './TodoCard';
 import Todo from '../types/todo';
 
-const myTodos: Array<Todo> = [
-  {
-    id: 1,
-    body: 'Buy milk for the family',
-    completed: false,
-  },
-  {
-    id: 2,
-    body: 'Finish extended project',
-    completed: false,
-  },
-  {
-    id: 3,
-    body: 'Take the trash out',
-    completed: false,
-  },
-];
+type Props = {
+  todos: Array<Todo>;
+};
 
-function TodoList() {
+function TodoList({ todos }: Props) {
   return (
     <section className='flex flex-col gap-2 px-6 border-l border-r mb-4'>
-      {myTodos.length > 0 ? (
-        myTodos.map((todo) => <TodoCard key={todo.id} body={todo.body} />)
+      {todos && todos.length > 0 ? (
+        todos.map((todo) => <TodoCard key={todo.id} body={todo.body} />)
       ) : (
         <p>No Todos were found...</p>
       )}
--- pages/api/todos.ts
+++ pages/api/todos.ts
@@ -1,12 +1,18 @@
-import { PrismaClient } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient, Todo } from '@prisma/client';
+import ITodo from '../../types/todo';
 
 const prisma = new PrismaClient();
 
+type Data = {
+  todos: Todo[];
+};
+
 // eslint-disable-next-line import/no-anonymous-default-export
-export default async (req: any, res: any) => {
+export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
   try {
     const todos = await prisma.todo.findMany();
-    return res.status(200).json(todos);
+    return res.status(200).json({ todos: todos });
   } catch (error) {
     console.error(error);
   }
